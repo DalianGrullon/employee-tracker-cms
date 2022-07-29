b99db4c72e256910dcf5c7998512efbcc3be8052
Dalian Grullon <dalian.business@aol.com>
2022-07-28 23:04:21 -0400

    successfully created department table in schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,10 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
