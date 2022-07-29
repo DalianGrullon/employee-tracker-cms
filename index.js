cc460b7552ce181158a2fd261214e708dd4ed1a8
Dalian Grullon <dalian.business@aol.com>
2022-07-28 22:56:11 -0400

    initialized database and created successful connection

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 
 require('dotenv').config();
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  },
+  console.log(`Connected to database.`)
+);
